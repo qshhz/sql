d01e9f50dd4213942dd50aae751b353f46a95f27
qshhz <gaigx01@163.com>
2015-10-01 15:09:36 -0500

    reformat

--- trycatch.sql
+++ trycatch.sql
@@ -1,62 +1,48 @@
---check to see whether this stored procedure exists.
-IF OBJECT_ID (N'usp_GetErrorInfo', N'P') IS NOT NULL
-DROP PROCEDURE usp_GetErrorInfo;
-GO
 
--- Create procedure to retrieve error information.
-CREATE PROCEDURE usp_GetErrorInfo
+ use testdb
+ GO
+
+CREATE PROCEDURE usp_TryCatch
 AS
-SELECT
-ERROR_NUMBER() AS ErrorNumber
-,ERROR_SEVERITY() AS ErrorSeverity
-,ERROR_STATE() AS ErrorState
-,ERROR_LINE () AS ErrorLine
-,ERROR_PROCEDURE() AS ErrorProcedure
-,ERROR_MESSAGE() AS ErrorMessage;
-GO
-
--- SET XACT_ABORT ON will cause the transaction to be uncommittable
--- when the constraint violation occurs.
+  -- SET XACT_ABORT ON will cause the transaction to be uncommittable
+  -- when the constraint violation occurs.
 SET XACT_ABORT ON;
-
+SET NOCOUNT ON;
+  
 BEGIN TRY
-BEGIN TRANSACTION;
--- A FOREIGN KEY constraint exists on this table. This
--- statement will generate a constraint violation error.
-DELETE FROM Production.Product
-WHERE ProductID = 980;
+  BEGIN TRANSACTION;
 
--- If the DELETE statement succeeds, commit the transaction.
-COMMIT TRANSACTION;
+  DELETE FROM Users
+  WHERE User_id = 1;
+  
+  COMMIT TRANSACTION;
 END TRY
 BEGIN CATCH
--- Execute error retrieval routine.
-EXECUTE usp_GetErrorInfo;
 
--- Test XACT_STATE:
--- If 1, the transaction is committable.
--- If -1, the transaction is uncommittable and should
---     be rolled back.
--- XACT_STATE = 0 means that there is no transaction and
---     a commit or rollback operation would generate an error.
+-- put the lower code into single USP
+    SELECT
+    ERROR_NUMBER() AS ErrorNumber
+    ,ERROR_SEVERITY() AS ErrorSeverity
+    ,ERROR_STATE() AS ErrorState
+    ,ERROR_LINE () AS ErrorLine
+    ,ERROR_PROCEDURE() AS ErrorProcedure
+    ,ERROR_MESSAGE() AS ErrorMessage;
+  
+  -- uncommittable.
+  IF (XACT_STATE()) = -1
+  BEGIN
 
--- Test whether the transaction is uncommittable.
-IF (XACT_STATE()) = -1
-BEGIN
-PRINT
-N'The transaction is in an uncommittable state.' +
-'Rolling back transaction.'
-ROLLBACK TRANSACTION;
-END;
-
--- Test whether the transaction is committable.
-IF (XACT_STATE()) = 1
-BEGIN
-PRINT
-N'The transaction is committable.' +
-'Committing transaction.'
-COMMIT TRANSACTION;
-END;
-END CATCH;
+    PRINT 'Rolling back transaction.'
+    ROLLBACK TRANSACTION;
+  END;
+  
+  -- committable.
+  IF (XACT_STATE()) = 1
+  BEGIN
+    PRINT 'Committing transaction.'
+    COMMIT TRANSACTION;
+  END;
+END CATCH
 GO
 
+EXEC usp_TryCatch
